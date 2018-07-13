ff855f9e039a3bfb97c17e23d01499fd35be3a13
James Henderson <hendersonjamesalexander@gmail.com>
2018-07-13 18:50:40 +0100

    character test created; all tests are passing

--- src/test/java/CharacterTest.java
+++ src/test/java/CharacterTest.java
@@ -1,2 +1,34 @@
+import db.DBHelper;
+import models.Actor;
+import models.Character;
+import models.Play;
+import models.Playwright;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
 public class CharacterTest {
+
+    Character character1;
+    Actor actor1;
+    Play play1;
+    Playwright playwright1;
+
+    @Before
+    public void before(){
+        playwright1 = new Playwright("William Shakespeare", "British");
+        play1 = new Play("Romeo and Juliet", 1754, 55000, playwright1);
+        character1 = new Character("Romeo Montague", play1);
+    }
+
+    @Test
+    public void hasName(){
+        assertEquals("Romeo Montague", character1.getName());
+    }
+
+    @Test
+    public void isInPlay(){
+        assertEquals(play1, character1.getPlay());
+    }
 }
