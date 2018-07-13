c1c5d95c2639aa23083edcf64bbc7b3762faf96f
James Henderson <hendersonjamesalexander@gmail.com>
2018-07-13 20:08:45 +0100

    Playwright tests passing

--- src/test/java/PlaywrightTest.java
+++ src/test/java/PlaywrightTest.java
@@ -1,2 +1,25 @@
+import models.Playwright;
+import org.junit.Before;
+import org.junit.Test;
+
+import static junit.framework.Assert.assertEquals;
+
 public class PlaywrightTest {
+
+    Playwright playwright1;
+
+    @Before
+    public void before(){
+        playwright1 = new Playwright("William Shakespeare", "British");
+    }
+
+    @Test
+    public void hasName(){
+        assertEquals("William Shakespeare", playwright1.getName());
+    }
+
+    @Test
+    public void hasNationality(){
+        assertEquals("British", playwright1.getNationality());
+    }
 }
